7a031711ce9f1aaa7ddb464599a6708a6606f297
wujun <wujun97@naver.com>
2020-12-14 01:03:50 +0900

    传输系统性能测试完成

---
+++ 解码器性能测试/test_for_res_file.py
@@ -0,0 +1,363 @@
+import sys
+import wave
+from threading import Thread
+import numpy as np
+import scipy.io as sio
+from PyQt5.QtCore import QThread
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import pyaudio
+from scipy.io.wavfile import write
+import sounddevice as sd
+import time
+import scipy.io.wavfile as wav
+import soundfile as sf
+from Messages import msgButtonClick, showDialog
+from scipy import signal
+import matplotlib.pyplot as plt
+import operator
+import csv
+
+form_class = uic.loadUiType("gui_all.ui")[0]
+
+sampling_rate = 48000
+symbol_duration = 0.025
+f = 4000
+N = sampling_rate * symbol_duration
+Ts = 1 / sampling_rate
+FILENAME = "tmp.wav"
+preamble = '01010101010101010101'
+
+preamble_signal = ','.join(list(preamble))
+preamble_time_signal = N * len(preamble) / sampling_rate
+preamble_t = np.arange(0, preamble_time_signal, Ts)
+preamble_np_signal = np.fromstring(preamble_signal, dtype='int', sep=',')
+preamble_sample = np.repeat(preamble_np_signal, N)
+preamble_y = np.sin(2 * np.pi * (f + preamble_sample * 2000) * preamble_t)
+
+
+def encode_c(s):
+    return ''.join([bin(ord(c)).replace('0b', '') for c in s])
+
+
+def decode_c(s):
+    return ''.join([chr(i) for i in [int(b, 2) for b in s.split(' ')]])
+
+
+class ThreadClass(QThread):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        global flag
+        flag = 0
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 1
+        frames = []
+
+        p = pyaudio.PyAudio()
+        stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=sampling_rate,
+                        input=True,
+                        output=True,
+                        frames_per_buffer=int(N))
+
+        while 1:
+            if flag == 0:
+                data = stream.read(int(N))
+                frames.append(data)
+            else:
+                break
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        wf = wave.open(FILENAME, "wb")
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setframerate(sampling_rate)
+        wf.writeframes(b"".join(frames))
+        wf.close()
+
+
+class WindowClass(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.encode_button.clicked.connect(self.encode)
+        self.decode_button.clicked.connect(self.decode)
+        self.start_record.clicked.connect(self.recording)
+        self.stop_record.clicked.connect(self.stop_recording)
+        self.load_csv.clicked.connect(self.load_csv_encoding)
+        self.play_wav.clicked.connect(self.play_wav_file)
+
+        self.threadclass = ThreadClass()
+
+
+
+    def play_wav_file(self):
+        filename = self.load_wav.toPlainText()
+        print(filename)
+        if filename == '':
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Information)
+            msgBox.setText("不能是空的")
+            msgBox.setWindowTitle("系统警告")
+            msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            msgBox.buttonClicked.connect(msgButtonClick)
+            returnValue = msgBox.exec()
+            if returnValue == QMessageBox.Ok:
+                return
+        try:
+            samplerate, data = sio.wavfile.read(filename)
+        except:
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Information)
+            msgBox.setText("没有该文件")
+            msgBox.setWindowTitle("系统警告")
+            msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            msgBox.buttonClicked.connect(msgButtonClick)
+            returnValue = msgBox.exec()
+            if returnValue == QMessageBox.Ok:
+                return
+        sd.play(data, samplerate)
+
+    def load_csv_encoding(self):
+        csv_content = []
+        f = open('content.csv', 'r', encoding='utf-8')
+        rdr = csv.reader(f)
+        i = 0
+        for line in rdr:
+            temp = ''
+            once = 0
+            i = i+ 1
+            if i < 6:
+                pass
+            else:
+                for i in range(int(line[2])):
+                    if once == 0:
+                        temp = line[2]
+                        once = 1
+                    else:
+                        temp = temp + line[4+i]
+                csv_content.append(temp)
+
+        print(csv_content)
+        print(len(csv_content))
+        f.close()
+
+    def decode(self):
+        fig = plt.figure(figsize=(10, 6))
+        display_result = ''
+        filtedData = []
+        current_length = 0
+        current_data = []
+        data = []
+        flag = 0
+        impulse_fft = []
+        impulse_fft_tmp = []
+        bin = []
+        real = []
+        self.decode_text.setPlainText(''.join(real))
+        f= open('result.csv', 'w', newline='')
+        csv_content = []
+        ff = open('content.csv', 'r', encoding='utf-8')
+        rdr = csv.reader(ff)
+        i = 0
+        for line in rdr:
+            temp = ''
+            once = 0
+            i = i + 1
+            if i < 6:
+                pass
+            else:
+                for i in range(int(line[2])):
+                    if once == 0:
+                        temp = line[2]
+                        once = 1
+                    temp = temp + line[4 + i]
+                wr = csv.writer(f)
+                wr.writerow(temp)
+
+        ff.close()
+
+        bandpass1 = 3000
+        bandpass2 = 7000
+        read_signal, fs = sf.read('res.wav')
+        plt.subplot(411)
+
+        plt.plot(read_signal[30000:120000])
+        plt.grid()
+
+        wn1 = 2.0 * bandpass1 / sampling_rate
+        wn2 = 2.0 * bandpass2 / sampling_rate
+        b, a = signal.butter(8, [wn1, wn2], 'bandpass')  # 범위를 조금 더 넓게 해야할
+
+        filtedData = signal.filtfilt(b, a, read_signal)  # data为要过滤的信号
+
+        plt.subplot(412)
+
+        plt.plot(filtedData[30000:120000])
+        plt.grid()
+
+        current_length = len(filtedData)
+        current_data = filtedData
+        while 1:
+            once_check = 0
+            corr = []
+            corr_index = dict()
+
+
+            for i in range(current_length - len(preamble_y)):
+                corr.append(np.corrcoef(current_data[i:i + len(preamble_y)], preamble_y)[0, 1])
+                if once_check + 57600 == i and once_check != 0:
+                    print('breakbreak')
+                    break
+
+                if corr[i] > 0.7:
+                    if once_check == 0:
+                        once_check = i
+                    print(i,'번째의 corr이',corr[i])
+
+                    corr_index[i] = corr[i]
+
+
+
+            try:
+                flag = max(corr_index.items(), key=operator.itemgetter(1))[0]
+            except:
+                print('decode 结束')
+                break
+
+
+            print(flag)
+            data = current_data[flag + len(preamble_y):len(current_data)]
+            plt.subplot(413)
+
+            plt.plot(data)
+            plt.grid()
+
+            target_fre = 6000
+            n = len(data)
+            window = 600
+            impulse_fft = np.zeros(n)
+            for i in range(int(n - window)):
+                y = np.fft.fft(data[i:i + int(window) - 1])
+                y = np.abs(y)
+                index_impulse = round(target_fre / sampling_rate * window)
+                impulse_fft[i] = max(y[index_impulse - 2:index_impulse + 2])
+
+            sliding_window = 10
+            impulse_fft_tmp = impulse_fft
+            for i in range(1 + sliding_window, n - sliding_window):
+                impulse_fft_tmp[i] = np.mean(impulse_fft[i - sliding_window:i + sliding_window])
+            impulse_fft = impulse_fft_tmp
+
+
+            decode_length = ''
+            for i in range(8):
+                bin = np.mean(impulse_fft[i * 1200:(i + 1) * 1200])
+                print(bin)
+                if bin < 100:
+                    decode_length = decode_length + '0'
+                else:
+                    decode_length = decode_length + '1'
+
+            print(decode_length)
+            decode_payload_length = int(decode_length, 2)
+            decode_payload = ''
+            for i in range(decode_payload_length):
+                bin = np.mean(impulse_fft[(i + 8) * 1200:(i + 1 + 8) * 1200])
+                if bin < 100:
+                    decode_payload = decode_payload + '0'
+                else:
+                    decode_payload = decode_payload + '1'
+
+            print(decode_payload)
+            writing = str(decode_payload_length)+decode_payload
+            wr = csv.writer(f)
+            wr.writerow(writing)
+            print(1200*(int(decode_length,2)+8))
+            current_data = current_data[1200*(int(decode_length,2)+8+20)+flag:len(current_data)]
+            current_length = len(current_data)
+            plt.subplot(414)
+
+            plt.plot(current_data)
+            plt.grid()
+            plt.show()
+            display_result = display_result + decode_payload
+
+
+        real = []
+        for i in range(int(len(display_result) / 7)):
+            real.append(decode_c(display_result[i * 7:(i + 1) * 7]))
+            print(real)
+        print(real)
+        self.decode_text.setPlainText(''.join(real))
+
+
+
+    def recording(self):
+        print('recording....')
+        self.threadclass.start()
+
+    def stop_recording(self):
+        global flag
+        flag = 1
+        print('stop!!!')
+
+    def encode(self):
+        input_text = encode_c(self.encode_text.toPlainText())
+        if input_text == '':
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Information)
+            msgBox.setText("不能是空的")
+            msgBox.setWindowTitle("系统警告")
+            msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            msgBox.buttonClicked.connect(msgButtonClick)
+            returnValue = msgBox.exec()
+            if returnValue == QMessageBox.Ok:
+                return
+        length = 40
+        temp = [input_text[i:i + length] for i in range(0, len(input_text), length)]
+        for i in range(len(temp)):
+            length_payload = str(format(len(temp[i]), 'b'))  # 7자리
+            if len(length_payload) < 8:
+                length_payload = length_payload.zfill(8)
+            total = preamble + length_payload + temp[i]
+            signal = ','.join(list(total))
+            time_signal = N * len(total) / sampling_rate
+            t = np.arange(0, time_signal, Ts)
+            np_signal = np.fromstring(signal, dtype='int', sep=',')
+            sample = np.repeat(np_signal, N)
+            if (len(t) % 10) != 0:
+                t = t[:len(t) - 1]
+            y = np.sin(2 * np.pi * (f + sample * 2000) * t)
+            write('first.wav', sampling_rate, y)
+            samplerate, data = sio.wavfile.read('first.wav')
+            sd.play(data, samplerate)
+            time.sleep(time_signal + 1)
+
+    def window(self):
+        app = QApplication(sys.argv)
+        win = QWidget()
+        button1 = QPushButton(win)
+        button1.setText("Show dialog!")
+        button1.move(50, 50)
+        button1.clicked.connect(showDialog)
+        win.setWindowTitle("Click button")
+        win.show()
+        sys.exit(app.exec_())
+
+    def msgButtonClick(self, i):
+        print("Button clicked is:", i.text())
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    myWindow = WindowClass()
+
+    myWindow.show()
+
+    app.exec_()
